7195f2e4da9150ca303fe6a868cebd87d72449a4
Angelakis <E.Angelakis@teledyne.com>
2025-03-11 14:29:27 +0000

    Added JS code and CSS Improvements
    Added JS code with arrays so that the selected options bring different words in the biscuit selection box every time the button is pressed.
    Also added CSS improvements.

--- Pass_The_Biscuit_Game.js
+++ Pass_The_Biscuit_Game.js
@@ -1,3 +1,34 @@
+const selectedOptions = {
+    CustardCreams: ["Surfing", "Gymnastics", "Spin", "Tennis", "Sprint", "Bend", "Painting", "Jump", "Walk", "Cycling", "Skip", "Crawl", "Football", "Cleaning", "Stretch"],
+    Bourbon: ["Slide", "Exercising", "Jog", "Meditating", "Basketball", "Martial Arts", "Swim", "Writing", "Climb", "Volleyball", "Drinking", "Shopping", "Cricket", "Dancing", "Skateboarding"],
+    RichTea: ["Cooking", "Swimming", "Baseball", "Reading", "Gardening", "Skiing", "Eating", "Golf", "Sleeping", "Roll", "Dive", "Driving"],
+    Digestives: ["Exercising", "Cooking", "Baseball", "Jump", "Bend", "Eating", "Cricket", "Cycling", "Skateboarding", "Basketball", "Shopping", "Football", "Surfing", "Sprint", "Martial Arts"],
+    Hobnobs: ["Cleaning", "Painting", "Sleeping", "Walk", "Crawl", "Gardening", "Writing", "Dive", "Stretch", "Volleyball", "Skip", "Slide", "Tennis", "Gymnastics", "Swimming"],
+    JammieDodgers: ["Dancing", "Swim", "Reading", "Meditating", "Skiing", "Climb", "Drinking", "Golf", "Spin", "Roll", "Jog"]
+};
+
+let currentIndex = 0;
+
+document.getElementById('Generate').addEventListener('click', function() {
+    const category = document.getElementById('operation').value;
+    const Result = document.getElementById('Result');
+    let items;
+
+    if (category === "All") {
+        items = [].concat(...Object.values(selectedOptions));
+        const randomIndex = Math.floor(Math.random() * items.length);
+        Result.innerText = items[randomIndex];
+    } else {
+        items = selectedOptions[category];
+        if (Result && items && items.length > 0) {
+            Result.innerText = items[currentIndex];
+            currentIndex = (currentIndex + 3) % items.length;
+        } else {
+            console.error('Element with id "Result" or selected category not found.');
+        }
+    }
+});
+
 let timeLeft = 120;
 const timerElement = document.getElementById('timer');
 const startButton = document.getElementById('startButton');
